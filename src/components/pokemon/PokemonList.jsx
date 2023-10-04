9f5c3f3c436e4da3b9a7ee3cd794eb4d6a2c7e3d
Brenna Clark <brennacclark@gmail.com>
2023-10-04 14:24:48 -0400

    Adding PokemonList and PokemonCard

---
+++ src/components/pokemon/PokemonList.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PokemonCard from "./PokemonCard"
+
+export default class   extends Component {
+  render() {
+    return (
+      <div className="row">
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard/>
+        <PokemonCard />
+      </div>
+    )
+  }
+}
