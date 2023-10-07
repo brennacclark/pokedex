1f12ae5942193f4a405e6d8c13bc54e1d513ddeb
Brenna Clark <brennacclark@gmail.com>
2023-10-07 17:58:56 -0400

    Updating card name to appear in front of sprite

--- src/components/pokemon/PokemonCard.jsx
+++ src/components/pokemon/PokemonCard.jsx
@@ -2,33 +2,47 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom'; 
 
+const Name = styled.h6`
+  z-index: 4;
+  position: relative;
+`;
+
+const ID = styled.h6`
+  z-index: 3;
+  position: relative; 
+`;
+
 const Sprite = styled.img`
   width: 3em;
   height: 5em;
-  transform: scale(2); /* Enlarge the image by 10% (5% on each side) */
-  z-index: 1; 
-
+  transform: scale(2);
+  z-index: 2;
+  position: relative;
 `;
 
 const Card = styled.div`
+  position: relative;
   color: white;
   text-decoration: none;
   background-color: #222;
   width: 120px;
   max-height: 100%;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   border-radius: 10px;
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-  font-weight: lighter; /* Set the font weight to a lighter value */
+  font-weight: lighter;
+  z-index: 1;
+
   &:hover {
-    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    box-shadow: 0 14px 28px rgba(0, 0, 0, .25), 
+                0 10px 10px rgba(0,0,0,.22);
   }
+  
   -moz-user-select: none;
   -webkit-user-select: none;
   user-select: none;
   -o-user-select: none;
 `;
-
 
 export default class PokemonCard extends Component {
   state = {
@@ -62,34 +76,39 @@ export default class PokemonCard extends Component {
   static cache = {}; // Static cache object for storing fetched data
 
   render() {
-  const { name } = this.props;
-  const { id, imageUrl } = this.state;
+    const { name } = this.props;
+    const { id, imageUrl } = this.state;
 
-  const nameParts = name.split('-').map(part => part.charAt(0).toUpperCase() + part.slice(1));
-  const formattedName = nameParts.join(' ');
+    const nameParts = name.split('-').map(part => part.charAt(0).toUpperCase() + part.slice(1));
+    const formattedName = nameParts.join(' ');
 
-  return (
-    <div className="pokemon-card" style={{ margin: '10px' }}>
-      <Link to={`/pokemon/${id}`}>
-        <Card className="card">
-          <h6 className="card-header" style={{ textAlign: 'right' }}>{id}</h6>
-          <Sprite
-            className=""
-            onLoad={() => this.setState({ imageLoading: false })}
-            onError={() => this.setState({ tooManyRequests: true })}
-            src={imageUrl}
-            style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }}
-          />
-          {/* {tooManyRequests ? (<h6>
-            <span className="badge badge-danger mt-2">Too Many Requests</span>
-          </h6>) : null} */}
-          <div className="" style={{ textAlign: 'center' }}>
-            <h6 className="card-title" style={{ fontSize: '10px'}}>{formattedName}</h6>
-          </div>
-        </Card>
-      </Link>
-    </div>
-  );
-}
+    return (
+      <div className="pokemon-card-frame" style={{ margin: '10px' }}>
+        <Link to={`/pokemon/${id}`}>
+          <Card className="card">
+            {/* ID */}
+            <ID className="card-header" style={{ textAlign: 'right' }}>{id}</ID>
 
+            <div className="sprite-frame">
+              {/* IMAGE */}
+              <Sprite
+                className=""
+                onLoad={() => this.setState({ imageLoading: false })}
+                onError={() => this.setState({ tooManyRequests: true })}
+                src={imageUrl}
+                style={{
+                  display: 'block',
+                  marginLeft: 'auto',
+                  marginRight: 'auto'
+                }}
+              />
+            </div>
+
+            {/*  NAME */}
+            <Name className="card-title" style={{ textAlign: 'center', fontSize: '10px' }}>{formattedName}</Name>
+          </Card>
+        </Link>
+      </div>
+    );
+  }
 }
